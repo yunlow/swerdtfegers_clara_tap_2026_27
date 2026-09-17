a02e6f9f1b67e330b0c2aa0bebc48294d72e25f1
yunlow <swerdtfegerslucas@gmail.com>
2026-09-17 11:34:38 +0200

    Ajout du logManager et LogEvent pour pouvoir stocker le message

---
+++ swerdtfegers_clara_tap_2026_27/activity_00_tap_26_27/Events/LogManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace activity_00_tap_26_27.Events
+{
+    public class LogManager
+    {
+        private EventManager _eventManager;
+       
+        public LogManager(EventManager event_manager)
+        {
+            _eventManager = event_manager;
+
+            _eventManager.RegisterToEvent<LogMessageGameEvent>(OnLogMessage);
+        }
+
+        private void OnLogMessage(IGameEvent game_event)
+        {
+            string log_entry = $"[{DateTime.Now}] {((LogMessageGameEvent)game_event).GetMessage()}";
+            File.AppendAllText("my_file.txt", log_entry);
+
+
+        }
+        public void Log(string message)
+        {
+            _eventManager.TriggerEvent(new LogMessageGameEvent(message));
+        }
+    }
+}
---
+++ swerdtfegers_clara_tap_2026_27/activity_00_tap_26_27/Events/LogMessageGameEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity_00_tap_26_27.Events
+{
+    public class LogMessageGameEvent : IGameEvent
+    {
+        private string _message;
+
+        public LogMessageGameEvent(string message)
+        {
+            _message = message;
+        }
+
+        public string GetMessage()
+        {
+            return _message;
+        }
+    }
+}
