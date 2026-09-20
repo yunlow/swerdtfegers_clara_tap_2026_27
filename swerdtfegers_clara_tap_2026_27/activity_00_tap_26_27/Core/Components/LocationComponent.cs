bf133fa2c6fa376ead8406a4c69017cf3b322405
yunlow <swerdtfegerslucas@gmail.com>
2026-09-20 22:55:54 +0200

    Création de la LocationComponent et de Connection

---
+++ swerdtfegers_clara_tap_2026_27/activity_00_tap_26_27/Core/Components/LocationComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity_00_tap_26_27.Core.Components
+{
+    public class LocationComponent : Component
+    {
+        private readonly string _locationName;
+        private readonly List<LocationLink> _links = new List<LocationLink>();
+
+        public LocationComponent(GameObject owner, string location_name)
+        {
+            _locationName = location_name;
+        }
+
+        public string GetLocationName()
+        {
+            return _locationName;
+        }
+
+        public void AddLink(GameObject destination, float duration)
+        {
+            _links.Add(new LocationLink(destination, duration));
+        }
+
+        public int GetDestinationCount()
+        {
+            return _links.Count;
+        }
+
+        public LocationLink GetDestinationAtIndex(int index)
+        {
+            if (index >= 0 && index < _links.Count)
+            {
+                return _links[index];
+            }
+            return null;
+        }
+    }
+}
---
+++ swerdtfegers_clara_tap_2026_27/activity_00_tap_26_27/Core/LocationLink.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity_00_tap_26_27.Core
+{
+    public class LocationLink
+    {
+        private readonly GameObject _destination;
+        private readonly float _duration;
+
+        public LocationLink(GameObject destination, float duration)
+        {
+            _destination = destination;
+            _duration = duration;
+        }
+
+        public GameObject GetDestination()
+        {
+            return _destination;
+        }
+
+        public float GetDuration()
+        {
+            return _duration;
+        }
+    }
+}
