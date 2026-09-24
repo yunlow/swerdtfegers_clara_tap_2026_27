e5c9346f30e277a9fe9165ac1418a97019e26656
yunlow <swerdtfegerslucas@gmail.com>
2026-09-24 10:34:02 +0200

    Création des tests unitaires

--- swerdtfegers_clara_tap_2026_27/ConsoleApp/ConsoleRenderManager.cs
+++ swerdtfegers_clara_tap_2026_27/ConsoleApp/ConsoleRenderManager.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace activity_00_tap_26_27.Presentation
+namespace activity_00_tap_26_27
 {
     public class ConsoleRenderManager
     {
---
+++ swerdtfegers_clara_tap_2026_27/GameLibraryTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace GameLibraryTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
