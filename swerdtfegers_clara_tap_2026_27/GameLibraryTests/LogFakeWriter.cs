56016cd48bd9deec1bff6b11e68e65e3b3b46f7b
yunlow <swerdtfegerslucas@gmail.com>
2026-09-24 12:14:23 +0200

    Mise en place de tests

---
+++ swerdtfegers_clara_tap_2026_27/GameLibraryTests/LogFakeWriter.cs
@@ -0,0 +1,16 @@
+﻿using GameLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLibraryTests
+{
+    public class LogFakeWriter : ILogWriter
+    {
+        private string _line;
+        public void WriteLine(string line)
+        {
+            _line = line;
+        }
+    }
+}
